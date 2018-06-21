321f1feb8b6235eef2979febc59b6c55349ee10b
Daniel Heidenhain <daniel_heidenhain@homedepot.com>
2018-06-21 17:14:44 -0400

    more stuff

--- schema/types/me.js
+++ schema/types/me.js
@@ -2,19 +2,33 @@ const {
   GraphQLID,
   GraphQLObjectType,
   GraphQLString,
+  GraphQLList,
   GraphQLNonNull
 } = require('graphql');
 
-const { fromSnakeCase } = require('../../lib/util');
+
+const pgdb = require('../../database/pgdb');
+const ContestType = require('./contest');
 
 module.exports = new GraphQLObjectType({
   name: 'MeType',
 
   fields: {
     id: { type: GraphQLID },
-    firstName: fromSnakeCase(GraphQLString),
-    lastName: fromSnakeCase(GraphQLString),
+    firstName: { type: GraphQLString },
+    lastName: { type: GraphQLString },
+    fullName: {
+      type: GraphQLString,
+      resolve: obj => `${obj.firstName} ${obj.lastName}`
+    },
     email: { type: new GraphQLNonNull(GraphQLString) },
-    createdAt: fromSnakeCase(GraphQLString)
+    createdAt: { type: GraphQLString },
+    contests: {
+      type: new GraphQLList(ContestType),
+      resolve(obj, args, { pgPool }) {
+        //read from db
+        return pgdb(pgPool).getContests(obj);
+      }
+    }
   }
-})
+});
