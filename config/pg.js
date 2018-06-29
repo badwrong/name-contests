00c5d6147cd55b702b7075e477ea038bf59fee3a
Daniel Heidenhain <daniel_heidenhain@homedepot.com>
2018-06-29 14:47:59 -0400

    deploying to gcp

--- config/pg.js
+++ config/pg.js
@@ -1,5 +1,11 @@
 module.exports = {
   development: {
     database: 'contests'
+  },
+  production: {
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_DATABASE,
+    host: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`
   }
 };
